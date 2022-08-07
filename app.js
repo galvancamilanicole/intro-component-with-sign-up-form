86456659a4ec28e0312bd6f9aebe19f744c908a0
galvancamilanicole <galvancamilanicole@gmail.com>
2022-08-07 16:36:15 -0300

    changes

--- app.js
+++ app.js
@@ -5,19 +5,24 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 // Routes
-var indexRouter = require('./routes/index');
+var indexRouter = require('./src/routes/index');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/src/views'));
 app.set('view engine', 'ejs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static('public'));
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto http://localhost:3000/');
+});
 
 app.use('/', indexRouter);
 
